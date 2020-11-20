5b7696c3b7ff0f23383dcaf94e6826d74ff6f7e2
Gaganpreet Singh <gaganpreetsingh9991@gmail.com>
2020-11-20 15:15:17 -0500

    calc updated

--- calc.sh
+++ calc.sh
@@ -1,11 +1,29 @@
 #! /usr/bin/env bash
-
+# An interactive calculator program
 # When the program is first loaded, display a greeting to the user.
+echo "Welcome to calculator program."
+# Keep prompting the user for input until they exit the program.
+while true; do
 # Then, display a menu that outlines the possible operations:
-  # Add
-  # Subtract
-  # Exit
-# Then, capture the user selection.
-# If the selection matches a supported operation, execute the operation.
-# If the selection does not match a support operation, display an error message.
-# When the operation is complete, redisplay the menu.
+  echo " Please select one of the following."
+    # Add
+    echo "Addition"
+    # Subtract
+    echo "Subtract"
+    # Multiplication
+    echo "Multiplication"
+    # Exit
+    echo "Exit"
+  # Then, capture the user selection.
+  read selection
+  case $selection in
+  # If the selection matches a supported operation, execute the operation.
+  "Addition")
+  echo "Please provide the first number:"
+  read first_number
+  echo "Please provide the second number:"
+  read second_number
+  echo "The sum of $first_number and $second_number is:"
+  expr $first_number + $second_number
+  ;;
+  "Subtraction")
